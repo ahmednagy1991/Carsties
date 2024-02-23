4ed82b3f684113e4259a6a36fd3da9fa44d5fefd
ahmed nagy <ahmed_nagy1991@hotmail.com>
2024-02-23 14:26:20 +0200

    creating search service

--- src/AuctionService/Controllers/AuctionController.cs
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -18,94 +18,94 @@ namespace AuctionService.Controllers
     {
 
         private readonly AuctionDBContext _context;
-    private readonly IMapper _mapper;
+        private readonly IMapper _mapper;
 
-    public AuctionController(AuctionDBContext context, IMapper mapper)
-    {
-        _context = context;
-        _mapper = mapper;
-    }
+        public AuctionController(AuctionDBContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
 
-    [HttpGet]
-    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
-    {
-        var auctions = await _context.Auctions
-            .Include(x => x.Item)
-            .OrderBy(x => x.Item.Make)
-            .ToListAsync();
+        [HttpGet]
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+        {
+            var auctions = await _context.Auctions
+                .Include(x => x.Item)
+                .OrderBy(x => x.Item.Make)
+                .ToListAsync();
 
-        return _mapper.Map<List<AuctionDto>>(auctions);
-    }
+            return _mapper.Map<List<AuctionDto>>(auctions);
+        }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
-    {
-        var auction = await _context.Auctions
-            .Include(x => x.Item)
-            .FirstOrDefaultAsync(x => x.Id == id);
-        
-        if (auction == null) return NotFound();
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+        {
+            var auction = await _context.Auctions
+                .Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
 
-        return _mapper.Map<AuctionDto>(auction);
-    }
+            if (auction == null) return NotFound();
 
-    [HttpPost]
-    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
-    {
-        var auction = _mapper.Map<Auction>(auctionDto);
-        // TODO: add current user as seller
-        auction.Seller = "test";
+            return _mapper.Map<AuctionDto>(auction);
+        }
 
-        _context.Auctions.Add(auction);
+        [HttpPost]
+        public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+        {
+            var auction = _mapper.Map<Auction>(auctionDto);
+            // TODO: add current user as seller
+            auction.Seller = "test";
 
-        var result = await _context.SaveChangesAsync() > 0;
+            _context.Auctions.Add(auction);
 
-        if (!result) return BadRequest("Could not save changes to the DB");
+            var result = await _context.SaveChangesAsync() > 0;
 
-        return CreatedAtAction(nameof(GetAuctionById), 
-            new {auction.Id}, _mapper.Map<AuctionDto>(auction));
-    }
+            if (!result) return BadRequest("Could not save changes to the DB");
 
-    [HttpPut("{id}")]
-    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
-    {
-        var auction = await _context.Auctions.Include(x => x.Item)
-            .FirstOrDefaultAsync(x => x.Id == id);
+            return CreatedAtAction(nameof(GetAuctionById),
+                new { auction.Id }, _mapper.Map<AuctionDto>(auction));
+        }
 
-        if (auction == null) return NotFound();
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+        {
+            var auction = await _context.Auctions.Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
 
-        // TODO: check seller == username
+            if (auction == null) return NotFound();
 
-        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
-        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
-        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
-        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
-        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+            // TODO: check seller == username
 
-        var result = await _context.SaveChangesAsync() > 0;
+            auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+            auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+            auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+            auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
 
-        if (result) return Ok();
+            var result = await _context.SaveChangesAsync() > 0;
 
-        return BadRequest("Problem saving changes");
-    }
+            if (result) return Ok();
 
-    [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteAuction(Guid id)
-    {
-        var auction = await _context.Auctions.FindAsync(id);
+            return BadRequest("Problem saving changes");
+        }
 
-        if (auction == null) return NotFound();
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAuction(Guid id)
+        {
+            var auction = await _context.Auctions.FindAsync(id);
 
-        // TODO: check seller == username
+            if (auction == null) return NotFound();
 
-        _context.Auctions.Remove(auction);
+            // TODO: check seller == username
 
-        var result = await _context.SaveChangesAsync() > 0;
+            _context.Auctions.Remove(auction);
 
-        if (!result) return BadRequest("Could not update DB");
+            var result = await _context.SaveChangesAsync() > 0;
 
-        return Ok();
-    }
+            if (!result) return BadRequest("Could not update DB");
+
+            return Ok();
+        }
 
     }
 }
---
+++ src/SearchService/Program.cs
@@ -0,0 +1,28 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await DB.InitAsync("SearchDB",
+MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDBConnection")));
+
+await DB.Index<Item>()
+.Key(x => x.Make, KeyType.Text)
+.Key(x => x.Model, KeyType.Text)
+.Key(x => x.Color, KeyType.Text)
+.CreateAsync();
+
+
+app.Run();
