9e3c153376820dc823c74d1eeca3c3808b2231b0
ahmed nagy <ahmed_nagy1991@hotmail.com>
2024-02-23 22:00:29 +0200

    Add filters

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
+using ZstdSharp.Unsafe;
 
 namespace SearchService.Controllers
 {
@@ -12,19 +14,72 @@ namespace SearchService.Controllers
     [Route("api/search")]
     public class SearchController : ControllerBase
     {
+        // [HttpGet]
+        // public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm,
+        //  int pageNumber = 1, int pageSize = 4)
+        // {
+        //     var query = DB.PagedSearch<Item>();
+        //     query.Sort(x => x.Ascending(a => a.Make));
+
+        //     if (!string.IsNullOrEmpty(searchTerm))
+        //     {
+        //         query.Match(Search.Full, searchTerm).SortByTextScore();
+        //     }
+        //     query.PageNumber(pageNumber);
+        //     query.PageSize(pageSize);
+        //     var result = await query.ExecuteAsync();
+        //     return Ok(new
+        //     {
+        //         result = result.Results,
+        //         pageCount = result.PageCount,
+        //         totalCount = result.TotalCount
+        //     });
+        // }
+
+
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm,
-         int pageNumber = 1, int pageSize = 4)
+        public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
         {
-            var query = DB.PagedSearch<Item>();
+            var query = DB.PagedSearch<Item, Item>();
             query.Sort(x => x.Ascending(a => a.Make));
 
-            if (!string.IsNullOrEmpty(searchTerm))
+            if (!string.IsNullOrEmpty(searchParams.SearchTerm))
             {
-                query.Match(Search.Full, searchTerm).SortByTextScore();
+                query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
             }
-            query.PageNumber(pageNumber);
-            query.PageSize(pageSize);
+
+
+
+            query = searchParams.OrderBy switch
+            {
+                "make" => query.Sort(x => x.Ascending(a => a.Make)),
+                "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+                _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+            };
+
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingsoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) &&
+                 x.AuctionEnd > DateTime.UtcNow),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+            };
+
+            if (!string.IsNullOrEmpty(searchParams.Seller))
+            {
+                query.Match(x => x.Seller == searchParams.Seller);
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.Winner))
+            {
+                query.Match(x => x.Winner == searchParams.Winner);
+            }
+
+
+            query.PageNumber(searchParams.PageNumber);
+            query.PageSize(searchParams.PageSize);
+
+
             var result = await query.ExecuteAsync();
             return Ok(new
             {
@@ -33,5 +88,7 @@ namespace SearchService.Controllers
                 totalCount = result.TotalCount
             });
         }
+
+
     }
 }
